1110080315ac1e8ea478ccde2f2e02f58b915927
Ritvikrocks <ritvikbhatnagar7@gmail.com>
2024-06-13 23:16:55 +0530

    learning javascript

---
+++ variable/script.js
@@ -0,0 +1,20 @@
+console.log("hey this is ritvik")
+let a=10;
+let b=30;
+console.log(a+b)
+let o={
+    "name":"ritvik",
+    "job":"web developer"
+}
+console.log(o)
+// o.salary="100 lakh"
+// console.log(typeof o)
+
+let age=10;
+if(age>=18)
+{
+    console.log("you are elligible to drive");
+}
+else{
+     console.log("you are underage and you cannot drive");
+}
