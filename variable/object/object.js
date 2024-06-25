0e737b1ff11f73c5dbbcd5250c8e541f8852be51
Ritvikrocks <ritvikbhatnagar7@gmail.com>
2024-06-26 01:08:16 +0530

    json

--- variable/object/object.js
+++ variable/object/object.js
@@ -12,3 +12,25 @@ let Js={
     // console.log(Js.full name) cannot be accessed using the dot, hence you generally access it using the brackets
     console.log(typeof(Js.mySym))//you cannot access the variavble using the dot, you need to use the bracket, the data type shows as string
     console.log(Js[mysym2])
+
+    //to copy one object to another
+    const target={a:1,b:2};
+    const source={b:4,c:5};
+    const obj4={...target,...source} //using the spread operator
+    const returned=Object.assign(target,source);
+    console.log(returned) 
+    console.log(obj4);
+
+    //if u get data from database
+    console.log(Object.keys(obj4));//you get output in array, on whicb u can easily apply the loop
+
+
+const course={
+    coursename:" javascript chai with code",
+    teacher:"hitesh choudary",
+    student:"ritvik the smart boy"
+}
+//course.coursename this is the correct way to call an object but there is anothe syntax
+//const{ the value u want to extract}=kahan se extract krni hai
+const{teacher:teacher}=course//destructuring the object and array can be done same.
+console.log(teacher );
